6ff98b2cfaec728fe09392da15393a68848168db
Mohamed Younis <myounis@ualberta.ca>
2025-08-23 07:56:41 -0600

    handle recipe id out of bounds

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -12,22 +12,24 @@ async def get_recipes():
 
 @app.get("/recipes/{recipe_id}")
 async def get_recipe(recipe_id: int):
-    return recipes[recipe_id]
+    if recipe_id > len(recipes) or recipe_id < 1:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipes[recipe_id - 1]
 
 
 recipes = [
     {
-        "id": 0,
+        "id": 1,
         "name": "Spaghetti Bolognese",
         "ingredients": ["spaghetti", "ground beef", "tomato sauce", "onion", "garlic"],
     },
     {
-        "id": 1,
+        "id": 2,
         "name": "Pancakes",
         "ingredients": ["flour", "milk", "egg", "baking powder", "sugar"],
     },
     {
-        "id": 2,
+        "id": 3,
         "name": "Chicken Salad",
         "ingredients": ["chicken breast", "lettuce", "tomato", "cucumber", "dressing"],
     }
